5a949d3e3f11d9fe20b40fddcf7f6fe60707145e
Matthew Bond <matthew.randal.bond@gmail.com>
2016-10-06 17:32:14 -0700

    init commit

---
+++ SimpleCalc/SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Matt Bond on 10/6/16.
+//  Copyright © 2016 Matt Bond. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
